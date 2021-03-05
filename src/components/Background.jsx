3984c5e3a7eb508b82f7a533ef4cae1540f33d26
leo <leo.kalshteyn@gmail.com>
2021-03-04 20:33:19 -0800

    background styling added

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,3 +4,25 @@ const Background = () => {
 };
 
 export default Background;
+
+
+import React from 'react';
+
+const Background = () => {
+  const backgroundStyle = {
+    fill: '#30abef',
+  };
+  const backgroundWidth = 5000;
+  const gameHeight = 1200;
+  return (
+    <rect
+      style={backgroundStyle}
+      x={skyWidth / -2}
+      y={100 - gameHeight}
+      width={backgroundWidth}
+      height={gameHeight}
+    />
+  );
+};
+
+export default Background;
