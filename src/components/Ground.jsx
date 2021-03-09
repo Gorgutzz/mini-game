d1f265c09998da5d975645675ec21144f5404371
leo <leo.kalshteyn@gmail.com>
2021-03-08 16:42:34 -0800

    add more styling for ground

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -12,7 +12,24 @@ const Ground = () => {
   const groundWidth = 5000;
 
   return (
-
+    <g id="ground">
+      <rect
+        id="ground-2"
+        data-name="ground"
+        style={groundStyle}
+        x={groundWidth / -2}
+        y={0}
+        width={groundWidth}
+        height={100}
+      />
+      <line
+        x1={groundWidth / -2}
+        y1={0}
+        x2={groundWidth / 2}
+        y2={0}
+        style={division}
+      />
+    </g>
   );
 };
 
