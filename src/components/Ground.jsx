8b87daaed66f18237a306dfa156b9ae7baabfbf8
leo <leo.kalshteyn@gmail.com>
2021-03-08 16:42:09 -0800

    set width for ground

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Ground = () => {
+  const groundStyle = {
+    fill: '#59a941',
+  };
+  const division = {
+    stroke: '#458232',
+    strokeWidth: '3px',
+  };
 
+  const groundWidth = 5000;
+
+  return (
+
+  );
 };
 
 export default Ground;
