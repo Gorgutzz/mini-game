fd5ff6c981238d544687f0abeb1ac1afd6d9753c
leo <leo.kalshteyn@gmail.com>
2021-03-10 20:01:48 -0800

    added cannon component

---
+++ src/components/CannonBase.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { pathFromBezierCurve } from '../utils/formulas';
+
+const CannonBase = (props) => {
+  const cannonBaseStyle = {
+    fill: '#a16012',
+    stroke: '#75450e',
+    strokeWidth: '2px',
+  };
+
+  return (
+
+};
+
+export default CannonBase;
