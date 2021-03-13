908142e9f13d0fd2a1b43fa60708d337f01bc5eb
leo <leo.kalshteyn@gmail.com>
2021-03-12 20:17:16 -0800

    give dimensions and styling to cannon base

--- src/components/CannonBase.js
+++ src/components/CannonBase.js
@@ -8,8 +8,45 @@ const CannonBase = (props) => {
     strokeWidth: '2px',
   };
 
-  return (
+  const baseWith = 80;
+  const halfBase = 40;
+  const height = 60;
+  const negativeHeight = height * -1;
 
+  const cubicBezierCurve = {
+    initialAxis: {
+      x: -halfBase,
+      y: height,
+    },
+    initialControlPoint: {
+      x: 20,
+      y: negativeHeight,
+    },
+    endingControlPoint: {
+      x: 60,
+      y: negativeHeight,
+    },
+    endingAxis: {
+      x: baseWith,
+      y: 0,
+    },
+  };
+
+  return (
+    <g>
+      <path
+        style={cannonBaseStyle}
+        d={pathFromBezierCurve(cubicBezierCurve)}
+      />
+      <line
+        x1={-halfBase}
+        y1={height}
+        x2={halfBase}
+        y2={height}
+        style={cannonBaseStyle}
+      />
+    </g>
+  );
 };
 
 export default CannonBase;
