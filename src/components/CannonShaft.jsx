95a8fda5e1edf8e065933452eafa43f239e31e66
leo <leo.kalshteyn@gmail.com>
2021-03-12 20:34:21 -0800

    added cannon shaft component

---
+++ src/components/CannonShaft.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pathFromBezierCurve } from '../utils/formulas';
+
+const CannonShaft = (props) => {
+  const cannonPipeStyle = {
+    fill: '#999',
+    stroke: '#666',
+    strokeWidth: '2px',
+  };
+  const transform = `rotate(${props.rotation}, 0, 0)`;
+
+  return (
+  );
+};
+
+
+export default CannonShaft;
