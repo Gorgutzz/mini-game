ee3af2a8f0e95d15b463d14df12fc0b36679be1c
leo <leo.kalshteyn@gmail.com>
2021-03-19 17:00:58 -0700

    imported prop types for canvas to use angle and track mouse

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Background from './Background';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonShaft from './CannonShaft';
 
-const Canvas = () => {
+const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
-      id="aliens-go-home-canvas"
+      id="mini-game-canvas"
       preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
       <Background />
       <Ground />
-      <CannonShaft rotation={45} />
+      <CannonShaft rotation={props.angle} />
       <CannonBase />
     </svg>
   );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
