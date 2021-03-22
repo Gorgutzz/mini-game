38b60eedeafc56f0f5e39f4d6bfa10c33b74218d
leo <leo.kalshteyn@gmail.com>
2021-03-22 15:55:36 -0700

    import prop types and add score property for current score

--- src/components/CurrentScore.jsx
+++ src/components/CurrentScore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const CurrentScore = (props) => {
   const scoreStyle = {
@@ -8,8 +9,16 @@ const CurrentScore = (props) => {
   };
 
   return (
+    <g filter="url(#shadow)">
+      <text style={scoreStyle} x="300" y="80">
+        {props.score}
+      </text>
+    </g>
   );
 };
 
+CurrentScore.propTypes = {
+  score: PropTypes.number.isRequired,
+};
 
 export default CurrentScore;
