c9d2db19b452ee57d976bfe315dd6175d0ea15f3
leo <leo.kalshteyn@gmail.com>
2021-03-24 18:17:19 -0700

    added flying dino base component

---
+++ src/components/FlyingDinoBase.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FlyingDinoBase = (props) => {
+  const style = {
+    fill: '#979797',
+    stroke: '#5c5c5c',
+  };
+
+  return (
+  );
+};
+
+
+export default FlyingDinoBase;
