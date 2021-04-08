7ccb8f83fb3c1e57f19308e5a92947ed0029b0e4
leo <leo.kalshteyn@gmail.com>
2021-04-07 19:47:37 -0700

    add position in life comp

--- src/components/Life.jsx
+++ src/components/Life.jsx
@@ -2,32 +2,70 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { pathFromBezierCurve } from '../utils/formulas';
 
-const Heart = (props) => {
-  const heartStyle = {
+const Life = (props) => {
+  const LifeStyle = {
     fill: '#da0d15',
     stroke: '#a51708',
     strokeWidth: '2px',
   };
 
+  const leftSide = {
+    initialAxis: {
+      x: props.position.x,
+      y: props.position.y,
+    },
+    initialControlPoint: {
+      x: -20,
+      y: -20,
+    },
+    endingControlPoint: {
+      x: -40,
+      y: 10,
+    },
+    endingAxis: {
+      x: 0,
+      y: 40,
+    },
+  };
+
+  const rightSide = {
+    initialAxis: {
+      x: props.position.x,
+      y: props.position.y,
+    },
+    initialControlPoint: {
+      x: 20,
+      y: -20,
+    },
+    endingControlPoint: {
+      x: 40,
+      y: 10,
+    },
+    endingAxis: {
+      x: 0,
+      y: 40,
+    },
+  };
+
   return (
     <g filter="url(#shadow)">
       <path
-        style={heartStyle}
+        style={liftStyle}
         d={pathFromBezierCurve(leftSide)}
       />
       <path
-        style={heartStyle}
+        style={liftStyle}
         d={pathFromBezierCurve(rightSide)}
       />
     </g>
   );
 };
 
-Heart.propTypes = {
+Life.propTypes = {
   position: PropTypes.shape({
     x: PropTypes.number.isRequired,
     y: PropTypes.number.isRequired
   }).isRequired,
 };
 
-export default Heart;
+export default Life;
