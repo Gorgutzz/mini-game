1798f405d653833b551a00b719bbda69cbf9d3b1
leo <leo.kalshteyn@gmail.com>
2021-04-14 16:03:21 -0700

    refactor createUfos func

--- src/reducers/createUfos.js
+++ src/reducers/createUfos.js
@@ -1,22 +1,41 @@
 import {
-  createInterval, flyingObjectsStarterYAxis, maxFlyingObjects,
-  flyingObjectsStarterPositions
+  createInterval, ufosStarterYAxis, maxufos,
+  ufosStarterPositions
 } from '../utils/constants';
 
 export default (state) => {
   if ( ! state.gameState.started) return state;
 
   const now = (new Date()).getTime();
-  const { lastObjectCreatedAt, flyingObjects } = state.gameState;
+  const { lastObjectCreatedAt, ufos } = state.gameState;
+  const createNewObject = (
+    now - (lastObjectCreatedAt).getTime() > createInterval &&
+    ufos.length < maxufos
   );
 
   if ( ! createNewObject) return state;
 
+  const id = (new Date()).getTime();
+  const predefinedPosition = Math.floor(Math.random() * maxufos);
+  const flyingObjectPosition = ufosStarterPositions[predefinedPosition];
+  const newFlyingObject = {
+    position: {
+      x: flyingObjectPosition,
+      y: ufosStarterYAxis,
+    },
+    createdAt: (new Date()).getTime(),
+    id,
   };
 
   return {
     ...state,
     gameState: {
+      ...state.gameState,
+      ufos: [
+        ...state.gameState.ufos,
+        newFlyingObject
+      ],
+      lastObjectCreatedAt: new Date(),
     }
   }
 }
