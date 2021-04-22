48fbbd305b13ed24753678badad0f1c107ec6b64
leo <leo.kalshteyn@gmail.com>
2021-04-22 15:41:03 -0700

    made leaderboard method and default props in leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Login from './Login';
+import Rank from "./Rank";
 
 const Leaderboard = (props) => {
   const style = {
@@ -15,10 +17,33 @@ const Leaderboard = (props) => {
     cursor: 'default',
   };
 
+  let leaderboard = props.leaderboard || [];
+  leaderboard = leaderboard.sort((prev, next) => {
+    if (prev.maxScore === next.maxScore) {
+      return prev.name <= next.name ? 1 : -1;
+    }
+    return prev.maxScore < next.maxScore ? 1 : -1;
+  }).map((member, index) => ({
+    ...member,
+    rank: index + 1,
+    currentPlayer: member.id === props.currentPlayer.id,
+  })).filter((member, index) => {
+    if (index < 3 || member.id === props.currentPlayer.id) return member;
+    return null;
+  });
+
   return (
+    <g>
+      <text filter="url(#shadow)" style={leaderboardTitle} x="-150" y="-630">Leaderboard</text>
+      <rect style={style} x="-350" y="-600" width="700" height="330" />
+
+    </g>
   );
 };
 
-Leaderboard.propTypes = {
+Leaderboard.defaultProps = {
+  currentPlayer: null,
+  leaderboard: null,
+};
 
 export default Leaderboard;
