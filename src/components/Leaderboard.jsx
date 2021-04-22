5cfedbe196c63a68d2700e3543492591395d13c0
leo <leo.kalshteyn@gmail.com>
2021-04-22 15:39:10 -0700

    added leaderboard component

---
+++ src/components/Leaderboard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Leaderboard = (props) => {
+  const style = {
+    fill: 'transparent',
+    stroke: 'black',
+    strokeDasharray: '15',
+  };
+
+  const leaderboardTitle = {
+    fontFamily: '"Joti One", cursive',
+    fontSize: 50,
+    fill: '#88da85',
+    cursor: 'default',
+  };
+
+  return (
+  );
+};
+
+Leaderboard.propTypes = {
+
+export default Leaderboard;
